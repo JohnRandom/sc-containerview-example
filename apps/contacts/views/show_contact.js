ec26cd18ef61c4da825789b09c854654ffbbb5f2
Florian Görsdorf <florian@BlackBook.local>
2010-04-28 15:05:26 +0200

    removed not used buttons

--- apps/contacts/views/show_contact.js
+++ apps/contacts/views/show_contact.js
@@ -14,27 +14,11 @@ sc_require('views/contact');
 Contacts.showContactView = SC.View.extend(
 /** @scope Contacts.ContactView.prototype */ {
 
-	childViews: 'contactView editButton doneButton'.w(),
+	childViews: 'contactView'.w(),
 
 	contactView: Contacts.contactView.design({
 		layout: { top: 0, bottom: 24, left: 0, right: 0 },
 		contentBinding: "Contacts.contactController"
-	}),
-
-	editButton: SC.ButtonView.design({
-		layout: { bottom: 0, left: 0, width: 90, height: 24 },
-		title: "Edit",
-		isDefault: YES,
-		action: "showEditView"
-	}),
+	})
 	
-	doneButton: SC.ButtonView.design({
-		layout: { bottom: 0, left: 0, width: 90, height: 24 },
-		title: "Done",
-		isDefault: NO,
-		isEnabled: NO,
-		isVisible: NO,
-		action: "closeEditView"
-	}),
-
 });
